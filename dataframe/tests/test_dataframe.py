dbee4d4bfb90e2e2e2087286e71a85528fa268eb
Jason Yen <jasonyen@umich.edu>
2024-09-01 13:01:22 -0400

    Implement tail function

--- dataframe/tests/test_dataframe.py
+++ dataframe/tests/test_dataframe.py
@@ -19,3 +19,16 @@ def test_df_vertical_concat():
 # TODO: ALL - Implement tests for all other DataFrame methods
 # Use the test_df_vertical_concat test as a reference
 # Reference pytest documentation as necessary: https://docs.pytest.org/en/stable/contents.html
+
+def test_df_tail():
+    # Create Series for the first DataFrame
+    series1_a = Series([1, 2], name="A")
+    series1_b = Series([3, 4], name="B")
+    series1_c = Series([5, 6], name="C")
+    series1_d = Series([7, 8], name="D")
+    df1 = DataFrame([series1_a, series1_b, series1_c, series1_d])
+    
+    assert df1.tail(1).data == [[7, 8]]
+    assert df1.tail(2).data == [[5, 6], [7, 8]]
+    assert df1.tail(3).data == [[3, 4], [5, 6], [7, 8]]
+    assert df1.tail(4).data == [[1, 2], [3, 4], [5, 6], [7, 8]]
