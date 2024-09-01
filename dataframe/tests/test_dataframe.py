6d7e8b5d8e03e36e39f9b838ff14108d3512f1b5
Cyrus Desai <pseudonym112304@gmail.com>
2024-09-01 12:58:11 -0400

    Created head function and tests

--- dataframe/tests/test_dataframe.py
+++ dataframe/tests/test_dataframe.py
@@ -19,3 +19,20 @@ def test_df_vertical_concat():
 # TODO: ALL - Implement tests for all other DataFrame methods
 # Use the test_df_vertical_concat test as a reference
 # Reference pytest documentation as necessary: https://docs.pytest.org/en/stable/contents.html
+
+def test_df_head():
+    # Create Series for the first DataFrame
+    series1_a = Series([1, 2], name="A")
+    series1_b = Series([3, 4], name="B")
+    df1 = DataFrame([series1_a, series1_b])
+    
+    # Create Series for the second DataFrame
+    series2_c = Series([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], name="C")
+    series2_d = Series([5, 4, 7, 55, 9, 10, 11, 12, 13, 14, 15], name="D")
+    df2 = DataFrame([series2_c, series2_d])
+    
+    assert df1.head().data == [[1, 2], [3, 4]]
+    assert df1.head().columns == df1.columns
+
+    assert df2.head().data == [(5, 6, 7, 8, 9), (5, 4, 7, 55, 9)]
+    assert df2.head(3) == [(5, 6, 7), (5, 4, 7)]
